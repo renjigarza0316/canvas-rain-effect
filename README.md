c392574f2b9d42bbe763b47087769e895a4525b0
rhuel <63617583+rhuelgarza03@users.noreply.github.com>
2022-01-25 18:16:29 -0700

    Code update

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # canvas-rain-effect
-This is a loop animation that simulates rain using javascript and the html5 canvas. It's fairly simple, but it still manages to be a pretty effect.
-
-To run it, all you need to do is download both the html and js file and drag the html file into your web browser.
+This is an animation that simulates rain using javascript and the html5 canvas. It is a simple, pretty effect.
+In order to run it you need to download both the html and js file and open the html file in a web browser.
 
 created: March 29, 2020
--- rain.js
+++ rain.js
@@ -1,47 +1,65 @@
-let c = document.getElementById("canvas");
+let c = document.getElementById("main");
 let ctx = c.getContext("2d");
 
-// hard coding in 1080p lol
-c.width = 1920;
-c.height = 1080;
-//c.width = window.innerWidth;
-//c.height = window.innerHeight;
+c.height = window.innerHeight;
+c.width = window.innerWidth;
 
-let dropCount = 3000; // the total amount of raindrops to spawn in
+let drops = [];
 
-let levels = []; // array for drops, idk why I called it levels
-let widths = [1, 2];
-let heights = [20, 25, 15];
+function startAnimation(dropCount, velocity, width) {
+	drops = [];
 
-let spacing = 0;
+	let minHeight = 10;
+	let maxHeight = 25;
+	
+	for (i = 0; i < dropCount; i++) {
+		let current = {
+			name: "p" + i,
+			width: width,
+			height: Math.floor((Math.random() * maxHeight) + minHeight),
+			velocity: velocity,
+			x: Math.floor(Math.random() * c.width),
+			y: Math.floor(Math.random() * c.height),
+		};
+		drops[i] = current;
+	}
+	
+	function loop() {
+		ctx.clearRect(0, 0, c.width, c.height);
+		ctx.fillStyle = "#4a4a4a";
+	
+		for (i = 0; i < drops.length; i++) {
+			drops[i].y += drops[i].velocity;
 
-for (let i = 0; i < dropCount; i++) {
-    spacing += Math.round(Math.random() * 20); // spacing rule between raindrops
+			if (drops[i].y > c.height) {
+				drops[i].y = -maxHeight - 5;
+				drops[i].x = Math.floor(Math.random() * c.width);
+			}
+			if (drops[i].x > c.width) {
+				drops[i].x = 0;
+			}
 
-    let current = {name: "p", width: 2, height: 25, vel: Math.round(Math.random() * 25) + 10, x: c.width/2, y: c.height/2};
-    current.x = spacing;
-
-    current.width = widths[Math.round(Math.random() * widths.length)];
-    current.height = heights[Math.round(Math.random() * heights.length)];
-
-    current.name = "p" + i;
-    levels[i] = current;
+			ctx.fillRect(drops[i].x, drops[i].y, drops[i].width, drops[i].height);
+		}
+		
+		if (dropCount != drops.length) { return; }
+		if (velocity != drops[0].velocity) { return; }
+		if (width != drops[0].width) { return; }
+		
+		requestAnimationFrame(loop);
+	}
+	loop();
 }
 
-function loop() {
-    ctx.clearRect(0, 0, c.width, c.height);
+function callAnimation() {
+	document.getElementById("dropLabel").innerHTML = document.getElementById("dropCount").value;
+	document.getElementById("velocityLabel").innerHTML = document.getElementById("velocity").value;
+	document.getElementById("widthLabel").innerHTML = document.getElementById("width").value;
 
-    ctx.fillStyle = "#404040";
+	let dropCount = parseInt(document.getElementById("dropCount").value);
+	let velocity = parseInt(document.getElementById("velocity").value);
+	let width = parseInt(document.getElementById("width").value);
 
-    for (let i = 0; i < dropCount; i++) {
-        levels[i].y += levels[i].vel;
-        ctx.fillRect(levels[i].x, levels[i].y, levels[i].width, levels[i].height);
-
-        if (levels[i].y > c.height) {
-            levels[i].y = Math.floor(Math.random() * ((-3000) - (-100)) + (-100));
-            levels[i].x = Math.round(Math.random() * c.width);
-        }
-    }
-    requestAnimationFrame(loop); // fancy for repeat 60 times a second
+	startAnimation(dropCount, velocity, width); 
 }
-loop(); // initial loop call
+callAnimation();
