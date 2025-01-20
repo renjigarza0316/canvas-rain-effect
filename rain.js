b3088b4f392603a96f5eec164e549488c658186a
renjigarza0316 <renjigarza1@gmail.com>
2025-01-20 14:55:07 -0800

    added a height option

--- rain.js
+++ rain.js
@@ -1,16 +1,37 @@
-let c = document.getElementById("main");
-let ctx = c.getContext("2d");
+let canvas = document.getElementById("main");
+let ctx = canvas.getContext("2d");
 
-c.height = window.innerHeight;
-c.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
 let drops = [];
 
+const ClampingSpeed = 1.1;
+
+let minHeight = 10;
+let maxHeight = 25;
+
+const defaultvelocity = 0;
+
+function DoDropletRain(index, drop) {
+	// the modulus of even number and 2 is always going to be zero.
+	let modulus = index%2;
+	
+	if (modulus == 0) {
+		drop.y += drop.velocity * (ClampingSpeed + .2) - Math.floor(drop.height / 9);
+		// drop.x = Math.floor(Math.random() * canvas.width);
+	}
+	else { 
+		drop.y += drop.velocity * ClampingSpeed - Math.floor(minHeight / 9);
+
+		// drop.x = Math.floor(Math.random() * canvas.width);
+	};
+};
+
+
+
 function startAnimation(dropCount, velocity, width) {
 	drops = [];
-
-	let minHeight = 10;
-	let maxHeight = 25;
 	
 	for (i = 0; i < dropCount; i++) {
 		let current = {
@@ -18,33 +39,37 @@ function startAnimation(dropCount, velocity, width) {
 			width: width,
 			height: Math.floor((Math.random() * maxHeight) + minHeight),
 			velocity: velocity,
-			x: Math.floor(Math.random() * c.width),
-			y: Math.floor(Math.random() * c.height),
+			x: Math.floor(Math.random() * canvas.width),
+			y: Math.floor(Math.random() * canvas.height),
 		};
 		drops[i] = current;
 	}
-	
-	function loop() {
-		ctx.clearRect(0, 0, c.width, c.height);
-		ctx.fillStyle = "#4a4a4a";
-	
-		for (i = 0; i < drops.length; i++) {
-			drops[i].y += drops[i].velocity;
 
-			if (drops[i].y > c.height) {
-				drops[i].y = -maxHeight - 5;
-				drops[i].x = Math.floor(Math.random() * c.width);
-			}
-			if (drops[i].x > c.width) {
+
+
+	function loop() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle = "#4a4a4a";
+		// scrolls the rain down on the y axis according to the velocity
+		for (i = 0; i < drops.length; i++) {
+			// drops[i].y += drops[i].velocity;
+
+
+			DoDropletRain(i, drops[i])
+			// console.log(drops[i])
+
+			if(drops[i].x > canvas.width) {
 				drops[i].x = 0;
 			}
+
+			if(drops[i].y > canvas.height) drops[i].y = 0
 
 			ctx.fillRect(drops[i].x, drops[i].y, drops[i].width, drops[i].height);
 		}
 		
-		if (dropCount != drops.length) { return; }
-		if (velocity != drops[0].velocity) { return; }
-		if (width != drops[0].width) { return; }
+		if(dropCount != drops.length)return;
+		if(velocity != drops[0].velocity)return;
+		if(width != drops[0].width)return;
 		
 		requestAnimationFrame(loop);
 	}
@@ -52,13 +77,18 @@ function startAnimation(dropCount, velocity, width) {
 }
 
 function callAnimation() {
-	document.getElementById("dropLabel").innerHTML = document.getElementById("dropCount").value;
-	document.getElementById("velocityLabel").innerHTML = document.getElementById("velocity").value;
-	document.getElementById("widthLabel").innerHTML = document.getElementById("width").value;
-
 	let dropCount = parseInt(document.getElementById("dropCount").value);
 	let velocity = parseInt(document.getElementById("velocity").value);
 	let width = parseInt(document.getElementById("width").value);
+	let Height = parseInt(document.getElementById("Height").value);
+	
+	document.getElementById("dropLabel").innerHTML = String(dropCount);
+	document.getElementById("velocityLabel").innerHTML = String(velocity);
+	document.getElementById("widthLabel").innerHTML = String(width);
+	document.getElementById("HeightLabel").innerHTML = String(Height);
+
+	minHeight = Height;
+	maxHeight = Height + 15;
 
 	startAnimation(dropCount, velocity, width); 
 }
